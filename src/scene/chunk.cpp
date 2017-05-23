665d8a2e8fde5439775421675afe9f5afe163e01
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2017-05-23 18:55:24 +0200

    Actually re-calculated the required grid size correctly

--- src/scene/chunk.cpp
+++ src/scene/chunk.cpp
@@ -12,9 +12,9 @@ Chunk::Chunk(const ivec3 &coord,
              const compute::context &context,
              int lod)
         : samples(context, N+3, N+3, N+3, Scene::samples_format())
-        , edges_x(context, N+3, N+3, N+3, Scene::edges_format())
-        , edges_y(context, N+3, N+3, N+3, Scene::edges_format())
-        , edges_z(context, N+3, N+3, N+3, Scene::edges_format())
+        , edges_x(context, N+2, N+3, N+3, Scene::edges_format())
+        , edges_y(context, N+3, N+2, N+3, Scene::edges_format())
+        , edges_z(context, N+3, N+3, N+2, Scene::edges_format())
         , vertices(context, N+2, N+2, N+2, Scene::vertices_format())
         , lock()
         , coord(coord)
