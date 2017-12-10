ae608bbc3fd0308cca1980c8bcd3c0e5aeaa1d63
Mateusz Krawiec <mateusz.krawiec6@gmail.com>
2017-12-10 11:02:19 +0100

    Less eye-straining default passthrough shader

--- media/shaders/passthrough-fs.glsl
+++ media/shaders/passthrough-fs.glsl
@@ -3,5 +3,6 @@ in vec3 vs_position;
 in vec3 gs_normal;
 
 void main() {
-    out_color = vec4(gs_normal, 1);
+    const vec3 L = normalize(vec3(1,1,1));
+    out_color = vec4(gs_normal.xyz*abs(min(0.1, dot(gs_normal, -L))), 1);
 }
